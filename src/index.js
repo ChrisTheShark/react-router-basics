194feb7bde87d0dbc18e9edafa3918d1080f0173
cdyer <chris.dyer@lowes.com>
2017-03-17 14:35:12 -0400

    Adding routes.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 // Libs
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import routes from './routes';
 
 // CSS
 import './css/style.css';
 
-// Components
-import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
-import Courses from './components/Courses';
-import Teachers from './components/Teachers';
-
 // Render
-render((
-  <Router history={hashHistory}>
-    <Route path="/" component={Home} />
-    <Route path="about" component={About} />
-    <Route path="courses" component={Courses} />
-    <Route path="teachers" component={Teachers} />
-  </Router>
-), document.getElementById('root'));
+render(routes, document.getElementById('root'));
